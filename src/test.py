29958a98512487091ee475d2e0e04f08c6b95c3f
Subham Kedia <49448247+notsocommon15@users.noreply.github.com>
2022-03-16 14:13:18 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -10,12 +10,12 @@ class TestHello(unittest.TestCase):
     def test_hello(self):
         rv = self.app.get('/')
         self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'Hello World!\n')
+        self.assertEqual(rv.data, b'Hello Bro or Sis!\n')
 
     def test_hello_hello(self):
         rv = self.app.get('/hello/')
         self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'Hello World!\n')
+        self.assertEqual(rv.data, b'Hello Bro or Sis!\n')
 
     def test_hello_name(self):
         name = 'Jose Cavendish'
